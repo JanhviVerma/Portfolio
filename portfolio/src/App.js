d97095762e76a3235336ce287f25eca3c9ce04a8
JanhviVerma <janhvi.verma2509@gmail.com>
2024-08-27 17:29:29 +0000

    Set up basic React components for personal portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,25 @@
+// App.js
+import React from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <footer>
+        <p>&copy; 2024 Janhvi Verma. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>Hi, I'm Janhvi Verma, a 9th grade student from Indore, MP, India. I'm passionate about coding and enjoy various sports.</p>
+      <h3>Hobbies</h3>
+      <ul>
+        <li>Badminton</li>
+        <li>Taekwondo</li>
+        <li>Running</li>
+        <li>Swimming</li>
+        <li>Coding (when bored)</li>
+      </ul>
+    </section>
+  );
+}
+
+export default About;
